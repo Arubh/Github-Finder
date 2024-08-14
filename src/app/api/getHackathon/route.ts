bb16f05216b71a7e34a08fcd863de7ed7e5bb19d
Aditya <iit2022003@iiitl.ac.in>
2024-08-14 19:54:30 +0530

    removed error

--- src/app/api/getHackathon/route.ts
+++
@@ -1,9 +0,0 @@
-import connect from "@/utils/db";
-import Hackathon from "@/models/Hackathon";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  await connect();
-  const hackathons = await Hackathon.find();
-  return NextResponse.json({ hackathons });
-}
